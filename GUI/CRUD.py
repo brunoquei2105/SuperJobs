a4aa4f5882ce7d4565e45787be170f2522a16e93
brunoquei2105 <61791032+brunoquei2105@users.noreply.github.com>
2020-11-16 19:38:38 -0300

    Update CRUD.py

--- GUI/CRUD.py
+++ GUI/CRUD.py
@@ -1,20 +1,19 @@
 import mysql.connector
 from datetime import date
 from passlib.hash import pbkdf2_sha256 as cryp
-
-
-
+import pandas as pd
 
 connection = mysql.connector.connect(host='localhost', user='root', password='21050630', database='SuperJobs', charset='utf8')
 print('Conexão feita com Sucesso.')
 cursor = connection.cursor(dictionary=True)
 
-'''Teste'''
-#cursor.execute(f'INSERT INTO User (nome, sobrenome, email, data_nasc, cpf, senha) '
-               #f'VALUES ("jose", "joffre", "jj@gmail.com", "1991/03/24", "55599977700", "12345")')
-#connection.commit()
+def usuario_superjobs(email):
+    user = email.split('@')[0]
+    print(f'Usuário criando com suesso: {user}')
 
+# Inserindo dados na Tabela User do BD SuperJobs
 resposta = ''
+
 while resposta != 'sair':
     print('***Bem Vindo ao Super Jobs***')
     nome = str(input('Digite o Nome:'))
@@ -23,22 +22,44 @@ while resposta != 'sair':
     data_nasc = str(input('Data de nascimento [AAAA/MM/DD]: '))
     cpf = str(input('Digite o Cpf: '))
     senha = str(input('Digite a Senha: '))
-    senha_encriptada = cryp.__hash__(senha, rounds=200000, salt_size=16)
-    cursor.execute(f'INSERT INTO User (nome, sobrenome, email, data_nasc, cpf, senha) VALUES ("{nome}", "{sobrenome}", "{email}",'
-                   f' "{data_nasc}", "{cpf}", "{senha_encriptada}")')
+    senha_encriptada = cryp.hash(senha, rounds=200000, salt_size=16)
+
+    cursor.execute(f'INSERT INTO User (nome, sobrenome, email, data_nasc, cpf, senha, usuario_name) VALUES ("{nome}", "{sobrenome}", '
+                   f'"{email}","{data_nasc}", "{cpf}", "{senha_encriptada}","{usuario_superjobs(email)}")')
     connection.commit()
     resposta = input('Digite sair para terminar.')
 
 current_date = date.today()
 formatted_date = current_date.strftime('%d/%m/%Y')
+print(current_date)
 
 # COMANDO DE SELEÇÃO
-sql_select = 'SELECT * FROM User;'
-cursor.execute(sql_select)
-for values in cursor:
-    print(values)
+select = input('Você deseja visualizar a Tabela User?')
+if select == 'Sim' or select == 'sim':
+    sql_select = 'SELECT * FROM User;'
+    cursor.execute(sql_select)
+    data_frame = pd.DataFrame(cursor)
+    print(data_frame.head())
+else:
+    print('Até Logo.')
 
 
+
+# UPDATE
+update = input('Deseja atualizar o dado de alguma coluna da Tabela User: ')
+if update == 'Sim' or update == 'sim':
+    columns = ['nome', 'sobrenome', 'email', 'dat_nasc', 'cpf', 'senha', 'usuario_name']
+    for column in columns:
+        print(column)
+    coluna = str(input('Qual das coluna deseja alterar: '))
+    valor = str(input('Digite a alteração que deseja fazer nesta coluna: '))
+    sql_update = f'UPDATE USER set {coluna} = {valor}'
+    cursor.execute(sql_update)
+else:
+    print('Até Logo.')
+
+
+# Fecha o curso, atualiza o BD e fecha a conexão
 cursor.close()
 connection.commit()
 connection.close()
